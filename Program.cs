b397c792ecc7f9792e268ee4f923f369597b2233
Andrew <andrew6594@gmail.com>
2022-12-13 14:41:01 +0300

    added the firt part

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Enter the number of the array {i}");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+}
